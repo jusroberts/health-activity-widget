cc3c4848acb664dcca7bf9f75aef68b14f39fe13
Justin Roberts <justin@wiggletonabbey.com>
2026-03-31 11:19:29 -0400

    Release v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wiggletonabbey.healthactivitywidget"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
